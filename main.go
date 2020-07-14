512a1efd685f332160facc6ed6842edef77c5936
KoutaKawase <suhrkawase@gmail.com>
2020-07-15 06:50:24 +0900

    feat: 🎸 有効なURLかどうか判定する機能作成

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"strings"
 
@@ -14,14 +15,36 @@ func action(c *cli.Context) error {
 		fmt.Println("No URL argument!")
 		return nil
 	}
-	url := c.Args().Get(0)
-	fmt.Println(url)
+
+	downloadURL := c.Args().Get(0)
+
+	if !IsValidURL(downloadURL) {
+		return cli.Exit("Invalid argument. Please input URL.", 86)
+	}
+
+	downloadURL = ConvertURL(downloadURL)
 	return nil
 }
 
+//IsValidURL github.com/.../blob/...じゃないURLを弾く
+func IsValidURL(downloadURL string) bool {
+	//ParseではなくParseRequestURIなのは絶対パスしか想定してないから
+	u, err := url.ParseRequestURI(downloadURL)
+
+	if err != nil {
+		return false
+	}
+
+	if u.Host == "github.com" && strings.Contains(u.Path, "/blob/") {
+		return true
+	}
+
+	return false
+}
+
 //ConvertURL 与えられたURLをダウンロード可能なものに変換する
-func ConvertURL(url string) string {
-	return strings.Replace(url, "/blob/", "/raw/", 1)
+func ConvertURL(downloadURL string) string {
+	return strings.Replace(downloadURL, "/blob/", "/raw/", 1)
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -2,11 +2,43 @@ package main
 
 import "testing"
 
+var downloadURL string = "https://github.com/mattn/go-gtk/blob/master/_example/demo/demo.go"
+
 func TestGitHubファイルURLがblobからrawに書き変えられているか(t *testing.T) {
-	url := ConvertURL("https://github.com/mattn/go-gtk/blob/master/_example/demo/demo.go")
+	converted := ConvertURL(downloadURL)
 	expected := "https://github.com/mattn/go-gtk/raw/master/_example/demo/demo.go"
 
-	if url != expected {
-		t.Errorf("欲しいのは %s。 でも %s だった", expected, url)
+	if converted != expected {
+		t.Errorf("欲しいのは %s。 でも %s だった", expected, converted)
+	}
+}
+
+func Test有効なGitHubURLならtrueを返すか(t *testing.T) {
+	isValid := IsValidURL(downloadURL)
+
+	if isValid != true {
+		t.Errorf("IsValidURL(%s) は trueが返ってこなければなりません", downloadURL)
+	}
+}
+
+func Test有効なワードが含まれてないURLならfalseを返すか(t *testing.T) {
+	cases := []struct {
+		in   string
+		want bool
+	}{
+		{"hogehoge", false},
+		{"https://example.com", false},
+		{"htt", false},
+		{"https://github.com/KoutaKawase", false},
+		{"https://blob/", false},
+		{"うおおおおおおｄさｋｆｊｋｓｄｌｊｆ", false},
+	}
+
+	for _, c := range cases {
+		got := IsValidURL(c.in)
+
+		if got != c.want {
+			t.Errorf("IsValidURL(%s) == %t, want %t", c.in, got, c.want)
+		}
 	}
 }
